80f4fc69853bd29c0c075c932768f02d963f3e5b
nickeduardos <nickeduardogk@gmail.com>
2026-09-13 15:06:31 -0300

    Realizado exercicio 02

--- exercicios.js
+++ exercicios.js
@@ -1,4 +1,4 @@
-// EXERCICIO 01
+// INICIO EXERCICIO 01
 
 let nomeUsuario
 let idadeUsuario
@@ -9,3 +9,19 @@ idadeUsuario = 21
 cidadeUsuario = "Não-Me-Toque"
 
 console.log(nomeUsuario + ", " + idadeUsuario + " anos, mora em " + cidadeUsuario + ".")
+
+// FIM EXERCICIO 01
+
+console.log ()
+
+// INICIO EXERCICIO 02
+
+let respostas = ["25", 25, true, null, undefined];
+
+console.log(typeof(respostas[0]))
+console.log(typeof(respostas[1]))
+console.log(typeof(respostas[2]))
+console.log(typeof(respostas[3]))
+console.log(typeof(respostas[4]))
+
+// FIM EXERCICIO 03
