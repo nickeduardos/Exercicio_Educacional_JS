8e32d51d047a1cbf83112d5db25ac56a68e5de79
nickeduardos <nickeduardogk@gmail.com>
2026-09-13 14:56:44 -0300

    primeiro exercicio realizado

---
+++ exercicios.js
@@ -0,0 +1,11 @@
+// EXERCICIO 01
+
+let nomeUsuario
+let idadeUsuario
+let cidadeUsuario
+
+nomeUsuario = "Nick"
+idadeUsuario = 21
+cidadeUsuario = "Não-Me-Toque"
+
+console.log(nomeUsuario + ", " + idadeUsuario + " anos, mora em " + cidadeUsuario + ".")
